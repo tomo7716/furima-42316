653312cca8eccba061c9930c01528e374fcf3c25
tomo7716 <t19870106.ak@gmail.com>
2025-07-14 11:25:35 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
