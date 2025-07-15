928e0b2920a290bf27c7afcdd484aca8a0dde267
tomo7716 <t19870106.ak@gmail.com>
2025-07-15 19:40:36 +0900

    商品出品ページを表示・保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 
@@ -8,4 +9,14 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
+  validates :image, presence: true
 end
