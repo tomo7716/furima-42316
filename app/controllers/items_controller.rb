d035589a00812cc5d8db377d6b41a9798d011321
tomo7716 <t19870106.ak@gmail.com>
2025-07-18 00:07:19 +0900

    商品一覧表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -16,6 +16,9 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
   end
 
   private
