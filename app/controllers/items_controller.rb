d28230d8b4997d474feccf10bf7a4f90528f3912
tomo7716 <t19870106.ak@gmail.com>
2025-07-22 12:39:11 +0900

    商品画面へ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -16,6 +16,14 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+   def show
+    @item = Item.find(params[:id])
   end
 
   private
