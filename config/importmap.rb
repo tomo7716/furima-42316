e2ee4b88373be23c6e371fe4addff75ead712283
tomo7716 <t19870106.ak@gmail.com>
2025-07-24 16:07:40 +0900

    デプロイログアウト設定(link_to変更)

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "item_price", to: "item_price.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3/lib/assets/compiled/rails-ujs.js"
