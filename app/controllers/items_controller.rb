31d8645fd6bbe719cb36cf6dd5fa03f04bbe29f4
tomo7716 <t19870106.ak@gmail.com>
2025-07-28 21:08:38 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item,only: [:edit, :update, :show]
+  before_action :set_item,only: [:edit, :update, :show, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
 
 
   private
