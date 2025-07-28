b29ee399664e4a5edc56b6f0b410cd07e4843e86
tomo7716 <t19870106.ak@gmail.com>
2025-07-28 21:43:13 +0900

    レビュー指摘事項修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
+  before_action :redirect_unless_owner, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,9 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return unless current_user.id != @item.user_id
-
-    redirect_to root_path
   end
 
   def show
@@ -53,5 +51,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
