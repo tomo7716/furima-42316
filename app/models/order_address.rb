a70565212c1febeab108ac04d1ad967e4a632ff6
tomo7716 <t19870106.ak@gmail.com>
2025-08-07 16:50:35 +0900

    レビュー指摘事項修正△2

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,16 +2,6 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code,     presence: true,
-                              format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-  validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" } # 都道府県でid:1は「--」などの無効値
-  validates :city,            presence: true
-  validates :house_number,    presence: true
-  validates :phone_number,    presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
-  validates :token,           presence: true
-  validates :user_id,         presence: true
-  validates :item_id,         presence: true
-
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
@@ -22,7 +12,7 @@ class OrderAddress
     validates :item_id
   end
 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }# 都道府県でid:1は「--」などの無効値
 
 
   def save
