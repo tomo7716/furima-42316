843d0ef5b4151a0f908af00639bf66a058e41210
tomo7716 <t19870106.ak@gmail.com>
2025-08-19 19:05:06 +0900

    元に戻す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-  # config.middleware.delete Rack::SSL if defined?(Rack::SSL)
+   config.force_ssl = true
+   config.middleware.delete Rack::SSL if defined?(Rack::SSL)
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
